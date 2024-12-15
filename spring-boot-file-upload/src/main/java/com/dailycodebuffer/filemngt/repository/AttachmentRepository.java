fa75db355a0308d222b3270aab48a97dece600c0
Ayushi_Sarkar <ayushisarkarcode@gmail.com>
2024-12-16 00:45:10 +0530

    comments added

--- spring-boot-file-upload/src/main/java/com/dailycodebuffer/filemngt/repository/AttachmentRepository.java
+++ spring-boot-file-upload/src/main/java/com/dailycodebuffer/filemngt/repository/AttachmentRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AttachmentRepository extends JpaRepository<Attachment, String> {
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
 }
 
+//Why Extend an Interface Instead of Writing Code?
+//When you extend JpaRepository, Spring Data JPA auto-generates the implementation of these methods at runtime.
+// You don't need to manually implement these methods, as Spring handles it dynamically.
 
--- spring-boot-file-upload/src/main/java/com/dailycodebuffer/filemngt/service/AttachmentService.java
+++ spring-boot-file-upload/src/main/java/com/dailycodebuffer/filemngt/service/AttachmentService.java
@@ -2,8 +2,13 @@ package com.dailycodebuffer.filemngt.service;
 
 import com.dailycodebuffer.filemngt.entity.Attachment;
 import org.springframework.web.multipart.MultipartFile;
-
+//It is part of the org.springframework.web.multipart package and
+// is commonly used in Spring MVC for receiving files from HTTP requests
+//When you upload a file using a form or API endpoint with multipart/form-data content type,
+//Spring binds the uploaded file to a MultipartFile object in the controller or service.
+// getName() , isEmpty(), etc ...
 import java.util.List;
+
 
 public interface AttachmentService {
     Attachment saveAttachment(MultipartFile file) throws Exception;
@@ -12,4 +17,7 @@ public interface AttachmentService {
 
     List<Attachment> searchFiles(String query);
 
+    List<Attachment> getAllFiles();
+
+    List<Attachment> filterFilesByType(String type);
 }
